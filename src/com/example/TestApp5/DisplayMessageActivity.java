125d9fc5c5589b1170b4d430a94d7d7189f01108
Asra Nizami <anizami@macalester.edu>
2014-02-16 17:43:35 -0600

    Tested out some more code

---
+++ src/com/example/TestApp5/DisplayMessageActivity.java
@@ -0,0 +1,42 @@
+package com.example.TestApp5;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Build;
+import android.annotation.SuppressLint;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Created by anizami on 2/16/14.
+ */
+public class DisplayMessageActivity {
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- src/com/example/TestApp5/MyActivity.java
+++ src/com/example/TestApp5/MyActivity.java
@@ -2,8 +2,14 @@ package com.example.TestApp5;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     /**
      * Called when the activity is first created.
      */
@@ -11,7 +17,16 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
 
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity2.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
 
+        // Do something in response to button
     }
 }
